378e8baeaf3dbb05e9075f07fd7be87b4ad83d58
DobrianskyiVol <dobrianskyi.volodymyr@chnu.edu.ua>
2024-05-17 20:41:39 +0300

    Add project files.

---
+++ My_future/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace My_future.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
